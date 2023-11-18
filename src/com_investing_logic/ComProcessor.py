06eca311b1c69f94b8e9cc1fdd0f77b5fdcd53a5
SPregnolato <s.pregnolato@pm.me>
2023-11-19 00:09:29 +0100

    added relative com

--- src/com_investing_logic/ComProcessor.py
+++ src/com_investing_logic/ComProcessor.py
@@ -4,11 +4,17 @@ import numpy as np
 class ComProcessor:
     """_summary_"""
 
-    def __init__(self, price_array: np.ndarray, qty_array: np.ndarray):
+    def __init__(
+        self, current_price: float, price_array: np.ndarray, qty_array: np.ndarray
+    ):
+        self.current_price = current_price
         self.price_array = price_array
         self.qty_array = qty_array
 
     def calculate_com(self) -> float:
-        # Method to process inputs, can be extensive
-        # ...
-        return np.dot(self.price_array, self.qty_array) / sum(self.qty_array)
+        self.com = np.dot(self.price_array, self.qty_array) / sum(self.qty_array)
+        return
+
+    def calculate_com_relative(self) -> float:
+        self.com_relative = ((self.current_price - self.com) / self.com) * 100
+        return
